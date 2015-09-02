4518ab16d278eb1bf799fed863f676388dcf0348
Samuel Ballan <sgb4622@gmail.com>
2015-09-01 22:53:30 -0400

    Begining to add jQuery

---
+++ script.js
@@ -0,0 +1,14 @@
+var randomNumer = Math.floor((Math.random() * 100) + 1);
+var guessCounter = 5;
+
+var inputNumber;
+
+$(document).ready(function() {	
+	$(".guess-counter").text("" + guessCounter + " Guesses Remaining");
+	
+	$("#numberguess").on()
+	
+	
+	
+});
+
