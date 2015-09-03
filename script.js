ec8f4d1feab28a7da87eb1e7ee5b7b84449c0f01
Samuel Ballan <sgb4622@gmail.com>
2015-09-03 15:33:42 -0400

    Got jQuery working, thanks Scott\!

--- script.js
+++ script.js
@@ -1,36 +1,51 @@
-var randomNumber = Math.floor((Math.random() * 100) + 1);
-var guessCounter = 5;
+/*jshint enforceall: false, strict: false, lastsemic: false, jquery: true*/
+
+var randomNumber;
+var guessCounter;
 var inputNumber;
-var started;
 
-var reset = function() {
-	guessCounter = 5;
+function reset() {
 	randomNumber = Math.floor((Math.random() * 100) + 1);
-	inputNumber = undefined;
-	setGuessCounter();
-	
-	alert("reset");
-	
+	guessCounter = 5;
+	inputNumber = 0;
 }
 
-var setGuessCounter = function(n) {
+function updateGuessCounter(n) {
+	if(n === undefined) { n= -1}
 	
+	guessCounter += n;
+	$(".guess-counter").text("" + guessCounter + " Guesses Remaining");
 }
 
-alert(guessCounter);
+function wrongAnswer() {
+	updateGuessCounter(-1);
+	if (guessCounter <= 0) { youLose() }
+}
+
+function youWin() {
+	alert("You Win!");
+}
+
+function youLose() {
+	alert("You Lose!");
+}
 
 $(document).ready(function() {
-	console.log("reloaded");
-	$("#play-again-button").click(reset);
+	reset();
+	alert("Random number is: " + randomNumber);
+	
+	$("#play-again-button").click(function() {
+		alert("Play Again!");
+		reset();
+	});
 	
 	$("#submit-button").on('click', function() {
 		inputNumber = $('#numberguess').val();
 		
 		if(inputNumber != randomNumber) {
-			console.log("reloaded");
-			guessCounter -= 1;
-			$(".guess-counter").text("" + guessCounter + " Guesses Remaining");
-			
+			wrongAnswer();
+		} else {
+			youWin();
 		}
 	});
 	
