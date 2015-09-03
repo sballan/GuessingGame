0776652509c3250fd9ec76e3f70158c0c4795c3b
Samuel Ballan <sgb4622@gmail.com>
2015-09-03 00:27:22 -0400

    Still a work in progress, continuing with jQuery study

--- script.js
+++ script.js
@@ -1,14 +1,40 @@
-var randomNumer = Math.floor((Math.random() * 100) + 1);
+var randomNumber = Math.floor((Math.random() * 100) + 1);
 var guessCounter = 5;
-
 var inputNumber;
+var started;
 
-$(document).ready(function() {	
-	$(".guess-counter").text("" + guessCounter + " Guesses Remaining");
+var reset = function() {
+	guessCounter = 5;
+	randomNumber = Math.floor((Math.random() * 100) + 1);
+	inputNumber = undefined;
+	setGuessCounter();
 	
-	$("#numberguess").on()
+	alert("reset");
 	
+}
+
+var setGuessCounter = function(n) {
+	
+}
+
+alert(guessCounter);
+
+$(document).ready(function() {
+	console.log("reloaded");
+	$("#play-again-button").click(reset);
+	
+	$("#submit-button").on('click', function() {
+		inputNumber = $('#numberguess').val();
+		
+		if(inputNumber != randomNumber) {
+			console.log("reloaded");
+			guessCounter -= 1;
+			$(".guess-counter").text("" + guessCounter + " Guesses Remaining");
+			
+		}
+	});
 	
 	
 });
+
 
